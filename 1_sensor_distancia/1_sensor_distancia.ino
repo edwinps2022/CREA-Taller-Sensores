bb1f58b061810025276bf2865ffdf036085e32e0
edwinps2022 <pulido.suarez.edwin.santiago@gmail.com>
2026-04-04 17:31:35 -0500

    Init: codigo incial para sensor de distancia

---
+++ 1_sensor_distancia/1_sensor_distancia.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20, 16, 2);
+
+int trig = 9;
+int echo = 10;
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  long tiempo;
+  int distancia;
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  tiempo = pulseIn(echo, HIGH);
+
+  lcd.setCursor(0,0);
+  lcd.print("Distancia:");
+
+  lcd.setCursor(0,1);
+
+  if(tiempo == 0){
+    lcd.print("Sin señal   ");
+  } else {
+    distancia = tiempo * 0.034 / 2;
+    lcd.print(distancia);
+    lcd.print(" cm   ");
+  }
+
+  delay(300);
+}
